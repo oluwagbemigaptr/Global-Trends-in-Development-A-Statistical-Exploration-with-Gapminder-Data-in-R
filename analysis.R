22e6292a815df8d08579bc5f8e8fcb05bcbf09ff
Peter OLUWAGBEMIGA <105968664+oluwagbemigaptr@users.noreply.github.com>
2025-04-24 08:48:05 +0100

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -1,1 +1,9 @@
+# Load necessary libraries
+library(tidyverse)
 
+# Load the Gapminder dataset from your local CSV file
+gapminder <- read.csv("gapminder_data.csv")
+
+# Quick check
+glimpse(gapminder)
+summary(gapminder)
